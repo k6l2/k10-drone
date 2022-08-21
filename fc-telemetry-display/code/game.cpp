e63a04b338e2aff9ebcee29dea4e23c798bc13a6
Kyle <ssjmetroid@gmail.com>
2022-08-21 14:19:15 -0700

    add bluetooth connect gui interface scaffold
    Had to log an issue in KORL, since the default font was not loading
    properly.

--- fc-telemetry-display/code/game.cpp
+++ fc-telemetry-display/code/game.cpp
@@ -13,6 +13,7 @@ KORL_GAME_API KORL_GAME_INITIALIZE(korl_game_initialize)
 {
     korl_memory_zero(fctd_memory, sizeof(*fctd_memory));
     fctd_memory->continueProgramExecution = true;
+    korl_gui_setFontAsset(L"submodules/korl/c/test-assets/source-sans/SourceSans3-Semibold.otf");
 }
 KORL_GAME_API KORL_GAME_ON_KEYBOARD_EVENT(korl_game_onKeyboardEvent)
 {
@@ -25,6 +26,12 @@ KORL_GAME_API KORL_GAME_ON_GAMEPAD_EVENT(korl_game_onGamepadEvent)
 }
 KORL_GAME_API KORL_GAME_UPDATE(korl_game_update)
 {
+    korl_gui_windowBegin(L"Connect Flight Controller Bluetooth", NULL, KORL_GUI_WINDOW_STYLE_FLAGS_DEFAULT);
+        if(korl_gui_widgetButtonFormat(L"Query Paired Bluetooth Devices"))
+        {
+            ///@TODO: ???
+        }
+    korl_gui_windowEnd();
     return fctd_memory->continueProgramExecution;
 }
 KORL_GAME_API KORL_GAME_ON_ASSET_RELOADED(korl_game_onAssetReloaded)
